0473d91b9e7b7c4b8377b15eb6d13feb6eae3264
Michaelzhouy <15602409303@163.com>
2020-04-28 22:43:42 +0800

    Update pandas.py

--- Python Tricks/Feature Engineering/pandas.py
+++ Python Tricks/Feature Engineering/pandas.py
@@ -1,16 +1,50 @@
-# 数据存储
-df.to_hdf('data.h5', 'df')
-pd.read_hdf('data.h5')
-
-# 筛选object特征
-df_object = df.select_dtypes(include=['object'])
-df_numerical = df.select_dtypes(exclude=['object'])
-
 import warnings
 warnings.filterwarnings('ignore')
 
 pd.set_option('max_columns', None)
 pd.set_option('max_rows', None)
+
+
+# 读文件
+def reduce_mem_usage(df):
+	"""
+	iterate through all the columns of a dataframe and modify the data type to reduce memory usage.
+	:param df:
+	:return:
+	"""
+	start_mem = df.memory_usage().sum()
+	print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+	for col in df.columns:
+		col_type = df[col].dtype
+
+		if col_type != object:
+			c_min = df[col].min()
+			c_max = df[col].max()
+			if str(col_type)[:3] == 'int':
+				if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+					df[col] = df[col].astype(np.int8)
+				elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+					df[col] = df[col].astype(np.int16)
+				elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+					df[col] = df[col].astype(np.int32)
+				elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+					df[col] = df[col].astype(np.int64)
+			else:
+				if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+					df[col] = df[col].astype(np.float16)
+				elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+					df[col] = df[col].astype(np.float32)
+				else:
+					df[col] = df[col].astype(np.float64)
+		else:
+			df[col] = df[col].astype('category')
+
+	end_mem = df.memory_usage().sum()
+	print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+	print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+	return df
+
 
 # 读取文件下所有的文件
 def read_data(path):
@@ -22,6 +56,15 @@ def read_data(path):
 		data_list.append(df)
 		del df
 		gc.collect()
-	
+
 	res = pd.concat(data_list, ignore_index=True)
 	return res
+
+
+# 数据存储
+df.to_hdf('data.h5', 'df')
+pd.read_hdf('data.h5')
+
+# 筛选object特征
+df_object = df.select_dtypes(include=['object'])
+df_numerical = df.select_dtypes(exclude=['object'])
