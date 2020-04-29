4efa435b766ba68de702cce12a771fe3167c934c
michaelzhouy <15602409303@163.com>
2020-04-29 17:10:03 +0800

    Update lgb.py

--- Python Tricks/lgb/lgb.py
+++ Python Tricks/lgb/lgb.py
@@ -4,12 +4,9 @@ params = {'boosting_type': 'gbdt',
           'objective': 'regression',
           'metric': 'mae',
           'learning_rate': 0.05}
-		  
-
-
 
 params = {'objective': 'binary',
-           'boosting': 'gbdt',          
+           'boosting': 'gbdt',
            'metric': 'auc',
            'num_iterations': 1000000,
            'learning_rate': 0.1,
@@ -26,8 +23,14 @@ params = {'objective': 'binary',
 lgb_train = lgb.Dataset(train_x, label=train_y)
 lgb_test = lgb.Dataset(test_x, label=test_y, reference=lgb_train)
 
-lgb.train(params,
-          lgb_train,
-          valid_sets=lgb_test,
-          early_stopping_rounds=200,
-          verbose_eval=300)
+lgb_model = lgb.train(params,
+                      lgb_train,
+                      valid_sets=lgb_test,
+                      early_stopping_rounds=200,
+                      verbose_eval=300)
+
+importance = lgb_model.feature_importance()
+feature_name = lgb_model.feature_name()
+
+feature_importance = pd.DataFrame({'feature_name': feature_name, 'importance': importance}).sort_values(by='importance', ascending=False)
+feature_importance.to_csv('feature_importance.csv', index=False)
