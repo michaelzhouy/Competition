b7235d6a91a9863cde539b9faf5519eefac27384
michaelzhouy <15602409303@163.com>
2020-05-02 12:18:36 +0800

    Update mean_encoding_wrong_demonstration.py

--- Python Tricks/Encoder/mean_encoding_wrong_demonstration.py
+++ Python Tricks/Encoder/mean_encoding_wrong_demonstration.py
@@ -3,6 +3,7 @@
 # @Author   : Michael_Zhouy
 
 import xgboost as xgb
+from sklearn.model_selection import StratifiedKFold
 
 means = X_tr.groupby(col).target.mean()
 train_new[col + '_mean_target'] = train_new[col].map(means)
@@ -20,3 +21,24 @@ model = xgb.train(xgb_par,
                   verbose_eval=30,
                   evals_result=evals_result,
                   early_stopping_rounds=50)
+
+
+# CV loop
+y_tr = df_tr['traget'].values  # target variable
+skf = StratifiedKFold(y_tr, 5, shuffle=True, random_state=123)
+
+for tr_ind, val_ind in skf:
+    X_tr, X_val = df_tr.iloc[tr_ind], df_tr.iloc[val_ind]
+    for col in cols:
+        means = X_val[col].map(X_tr.groupby(col).target.mean())
+        X_val[col + '_mean_target'] = means
+    train_new.iloc[val_ind] = X_val
+
+prior = df_tr['target'].mean()  # global mean
+train_new.fillna(prior, inplace=True)
+
+
+# Expanding mean
+cumsum = df_tr.groupby(col)['target'].cumsum() - df_tr['target']
+cumcnt = df_tr.groupby(col).cumcount()
+train_new[col + '_mean_target'] = cumsum / cumcnt
