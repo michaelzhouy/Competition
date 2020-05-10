27a6b2158c486740d4a41c1f8e8323c90e1e5bf1
Michaelzhouy <15602409303@163.com>
2020-05-10 19:13:21 +0800

    Update

---
+++ Python Tricks/Feature Engineering/feature_engineering.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+# Time   : 2020/5/10 18:50
+# Email  : 15602409303@163.com
+# Author : Zhou Yang
+
+import numpy as np
+import pandas as pd
+
+
+def overfit_reducer(df):
+    """
+    计算每列中取值的分布，返回单一值占比达99.74%的列名
+    @param df:
+    @return:
+    """
+    overfit = []
+    for i in df.columns:
+        counts = df[i].value_counts()
+        zeros = counts.iloc[0]
+        if zeros / len(df) * 100 > 99.94:
+            overfit.append(i)
+    return overfit
+
+
+# 筛选object特征
+df_object = df.select_dtypes(include=['object'])
+df_numerical = df.select_dtypes(exclude=['object'])
+
+# 将类别较少的取值归为一类
+name_freq = 2
+name_dict = dict(zip(*np.unique(df['name'], return_counts=True)))
+df['name'] = df['name'].apply(lambda x: -999 if name_dict[x] < name_freq else x)
+
+# 分组排名
+df.groupby('uid')['time'].rank('dense')
+
+# 根据时间划分训练集、验证集和测试集
+train = df.loc[df['observe_date'] < '2019-11-04', :]
+valid = df.loc[(df['observe_date'] >= '2019-11-04') & (df['observe_date'] <= '2019-12-04'), :]
+test = df.loc[df['observe_date'] > '2020-01-04', :]
--- Python Tricks/Feature Engineering/pandas.py
+++ Python Tricks/Feature Engineering/pandas.py
@@ -1,86 +1,16 @@
+# -*- coding:utf-8 -*-
+# Time   : 2020/5/10 19:12
+# Email  : 15602409303@163.com
+# Author : Zhou Yang
+
 import numpy as np
 import pandas as pd
 import os
+
+# 过滤警告
 import warnings
 warnings.filterwarnings('ignore')
 
+# DataFrame显示所有列
 pd.set_option('max_columns', None)
 pd.set_option('max_rows', None)
-
-# 分组排名
-df.groupby('uid')['time'].rank('dense')
-
-
-# 读文件
-def reduce_mem_usage(df):
-	"""
-	iterate through all the columns of a dataframe and modify the data type to reduce memory usage.
-	:param df:
-	:return:
-	"""
-	start_mem = df.memory_usage().sum()
-	print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
-
-	for col in df.columns:
-		col_type = df[col].dtype
-
-		if col_type != object:
-			c_min = df[col].min()
-			c_max = df[col].max()
-			if str(col_type)[:3] == 'int':
-				if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-					df[col] = df[col].astype(np.int8)
-				elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-					df[col] = df[col].astype(np.int16)
-				elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-					df[col] = df[col].astype(np.int32)
-				elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-					df[col] = df[col].astype(np.int64)
-			else:
-				if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-					df[col] = df[col].astype(np.float16)
-				elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
-					df[col] = df[col].astype(np.float32)
-				else:
-					df[col] = df[col].astype(np.float64)
-		else:
-			df[col] = df[col].astype('category')
-
-	end_mem = df.memory_usage().sum()
-	print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
-	print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
-	return df
-
-
-# 读取文件下所有的文件
-def read_data(path):
-	data_list = []
-	for f in os.listdir(path):
-		print(f)
-		df = pd.read_csv(path + os.sep + f)
-		print(df.shape)
-		data_list.append(df)
-		del df
-		gc.collect()
-
-	res = pd.concat(data_list, ignore_index=True)
-	return res
-
-
-# 数据存储为h5格式
-df.to_hdf('data.h5', 'df')
-pd.read_hdf('data.h5')
-
-# 筛选object特征
-df_object = df.select_dtypes(include=['object'])
-df_numerical = df.select_dtypes(exclude=['object'])
-
-# 将类别较少的取值归为一类
-name_freq = 2
-name_dict = dict(zip(*np.unique(data['name'], return_counts=True)))
-data['name'] = data['name'].apply(lambda x: -999 if name_dict[x] < name_freq else x)
-
-# 根据时间划分训练集、验证集和测试集
-train = df.loc[df['observe_date'] < '2019-11-04', :]
-valid = df.loc[(df['observe_date'] >= '2019-11-04') & (df['observe_date'] <= '2019-12-04'), :]
-test = df.loc[df['observe_date'] > '2020-01-04', :]
