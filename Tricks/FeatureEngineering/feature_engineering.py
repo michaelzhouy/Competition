d1a63cee1444e2f6fc435f2d1354ec38d56428c1
michaelzhouy <15602409303@163.com>
2020-07-29 20:34:38 +0800

    Update feature_engineering.py

--- Tricks/FeatureEngineering/feature_engineering.py
+++ Tricks/FeatureEngineering/feature_engineering.py
@@ -94,6 +94,30 @@ df_object = df.select_dtypes(include=['object'])
 df_numerical = df.select_dtypes(exclude=['object'])
 
 
+def get_same_set(train_df,test_df):
+    """
+    test中出现，train中没有出现的取值编码
+    @param train_df:
+    @param test_df:
+    @return:
+    """
+    train_diff_test = set(train_df) - set(test_df)
+    same = set(train_df) - train_diff_test
+    test_diff_train = set(test_df) - same
+    dic_ = {}
+    cnt = 0
+    for val in same:
+        dic_[val] = cnt
+        cnt += 1
+    for val in train_diff_test:
+        dic_[val] = cnt
+        cnt += 1
+    for val in test_diff_train:
+        dic_[val] = cnt
+        cnt += 1
+    return dic_
+
+
 def overfit_reducer(df):
     """
     计算每列中取值的分布，返回单一值占比达99.74%的列名
