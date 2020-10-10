c6d5a58d01d2e72357e6f06a4df02c1514ce428e
Michaelzhouy <15602409303@163.com>
2020-10-10 22:18:59 +0800

    Create main_predict.py

---
+++ 10-西湖论剑/02-加密流量检测/大数据队_eta_submission_1011/predict_code/main_predict.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+def test_func(test_path,save_path):
+	# 请填写测试代码
+	test = pd.read_csv(test_path)
+	# 选手不得改变格式，测试代码跑不通分数以零算
+
+	# #####选手填写测试集处理逻辑,在指定文件夹下生成可提交的csv文件
+
+	# demo#
+	submission = test[['eventId']]
+	submission['label'] = 0
+	submission.to_csv(save_path + '大数据队_eta_submission_1011.csv',index = False,encoding='utf-8')
+
+
+if __name__ == '__main__':
+    test_path = '../data/test_1.csv'
+    sava_path = '../results/'
+	test_func(test_path,sava_path)
