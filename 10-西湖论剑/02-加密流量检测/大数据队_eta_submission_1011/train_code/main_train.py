9cd299426f6015150d17b51493c15a63a6478e7e
Michaelzhouy <15602409303@163.com>
2020-10-10 22:19:39 +0800

    Create main_train.py

---
+++ 10-西湖论剑/02-加密流量检测/大数据队_eta_submission_1011/train_code/main_train.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+
+
+def train_func(train_path):
+#请填写训练代码
+    pass
+
+if __name__ == '__main__':
+    train_path = '../data/train.csv'
+    train_func()
