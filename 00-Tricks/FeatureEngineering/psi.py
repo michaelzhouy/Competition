e8239970bcbdee6ed3a12a9427a992d1c92b8337
michaelzhouy <15602409303@163.com>
2020-10-15 14:29:41 +0800

    Update psi.py

--- 00-Tricks/FeatureEngineering/psi.py
+++ 00-Tricks/FeatureEngineering/psi.py
@@ -6,44 +6,47 @@ import numpy as np
 import pandas as pd
 import math
 from tqdm import tqdm
+from joblib import Parallel, delayed
 
 
 # 调用方法
-# psi_res,psi_dict = get_psi(train, test2, f_cols)
-def get_psi(train,test,f_cols):
+# psi_res = Parallel(n_jobs=4)(delayed(get_psi)(c) for c in tqdm(features))
+# psi_df = pd.concat(psi_res)
+# features = list(psi_df[psi_df['PSI'] <= 0.2]['变量名'].values)
+def get_psi(c):
     psi_res = pd.DataFrame()
-    psi_dict ={}
-    for c in tqdm(f_cols):
-        try:
-            t_train = train[c].fillna(-998)
-            t_test = test[c].fillna(-998)
-            #获取切分点
-            bins=[]
-            for i in np.arange(0,1.1,0.1):
-                bins.append(t_train.quantile(i))
-            bins=sorted(set(bins))
-            bins[0]=-np.inf
-            bins[-1]=np.inf
-            #计算psi
-            t_psi = pd.DataFrame()
-            t_psi['train'] = pd.cut(t_train,bins).value_counts().sort_index()
-            t_psi['test'] = pd.cut(t_test,bins).value_counts()
-            t_psi.index=[str(x) for x in t_psi.index]
-            t_psi.loc['总计',:] = t_psi.sum()
-            t_psi['train_rate'] = t_psi['train']/t_psi.loc['总计','train']
-            t_psi['test_rate'] = t_psi['test']/t_psi.loc['总计','test']
-            t_psi['psi'] = (t_psi['test_rate']-t_psi['train_rate'])*(np.log(t_psi['test_rate'])-np.log(t_psi['train_rate']))
-            t_psi.loc['总计','psi'] = t_psi['psi'].sum()
-            t_psi.index.name=c
-            #汇总
-            t_res = pd.DataFrame([[c,t_psi.loc['总计','psi']]],
-                                 columns=['变量名','PSI'])
-            psi_res = pd.concat([psi_res,t_res])
-            psi_dict[c]=t_psi
-            print(c,'done')
-        except:
-            print(c,'error')
-    return psi_res, psi_dict
+    psi_dict={}
+    # for c in tqdm(f_cols):
+    try:
+        t_train = x_train[c].fillna(-998)
+        t_test = x_test[c].fillna(-998)
+        #获取切分点
+        bins=[]
+        for i in np.arange(0,1.1,0.2):
+            bins.append(t_train.quantile(i))
+        bins=sorted(set(bins))
+        bins[0]=-np.inf
+        bins[-1]=np.inf
+        #计算psi
+        t_psi = pd.DataFrame()
+        t_psi['train'] = pd.cut(t_train,bins).value_counts().sort_index()
+        t_psi['test'] = pd.cut(t_test,bins).value_counts()
+        t_psi.index=[str(x) for x in t_psi.index]
+        t_psi.loc['总计',:] = t_psi.sum()
+        t_psi['train_rate'] = t_psi['train']/t_psi.loc['总计','train']
+        t_psi['test_rate'] = t_psi['test']/t_psi.loc['总计','test']
+        t_psi['psi'] = (t_psi['test_rate']-t_psi['train_rate'])*(np.log(t_psi['test_rate'])-np.log(t_psi['train_rate']))
+        t_psi.loc['总计','psi'] = t_psi['psi'].sum()
+        t_psi.index.name=c
+        #汇总
+        t_res = pd.DataFrame([[c,t_psi.loc['总计','psi']]],
+                             columns=['变量名','PSI'])
+        psi_res = pd.concat([psi_res,t_res])
+        psi_dict[c]=t_psi
+        print(c,'done')
+    except:
+        print(c,'error')
+    return psi_res #, psi_dict
 
 
 def data_month_psi(df, nameks):
