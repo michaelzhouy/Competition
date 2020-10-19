be64f9b688c46a5371f503024ac9257f50e2a43f
michaelzhouy <15602409303@163.com>
2020-10-19 15:54:04 +0800

    Create lgb_regression.py

---
+++ 00-Tricks/lgb/lgb_regression.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2020/10/19 14:19
+# @Author   : Michael_Zhouy
+
+
+# regression
+params = {
+    'boosting_type': 'gbdt',
+    'objective': 'regression',
+    'metric': 'mae',
+    'learning_rate': 0.05,
+    'seed': 2020
+}
