9a50530f4f76c987d73cc6fb5397bac7ec0321cc
michaelzhouy <15602409303@163.com>
2020-10-22 18:07:44 +0800

    Update encoder.py

--- 00-Tricks/FeatureEngineering/encoder.py
+++ 00-Tricks/FeatureEngineering/encoder.py
@@ -10,9 +10,7 @@ import gc
 from sklearn.preprocessing import LabelEncoder
 
 
-le = LabelEncoder()
-le.fit(X)
-le_dict = dict(zip(le.classes_, le.transform(le.classes_)))
+
 
 
 def low_freq_encode(df, cat_cols, freq=2):
@@ -92,6 +90,38 @@ def train_test_label_encode(df, cat_col, type='save', path='./'):
         np.save(path + '{}.npy'.format(cat_col), d)
         return df
     elif type == 'load':
+        d = np.load(path + '{}.npy'.format(cat_col), allow_pickle=True).item()
+        return d
+
+
+def train_test_label_encode_2(df, cat_col, type='save', path='./'):
+    """
+    train和test分开label encode
+    save的食用方法
+    for i in cat_cols:
+        train = train_test_label_encode_2(train, i, 'save', './')
+        train[i] = train[i].astype('category')
+    load的食用方法：
+    for i in cat_cols:
+        d = train_test_label_encode_2(test, i, 'load', '../train_code/')
+        test[i] = test[i].map(d)
+        test[i] = test[i].astype('category')
+    @param df:
+    @param cat_col:
+    @param type: 'save' 'load'
+    @param path:
+    @return:
+    """
+    if type == 'save':
+        print(cat_col)
+        le = LabelEncoder()
+        le.fit(df[cat_col])
+        le_dict = dict(zip(le.classes_, le.transform(le.classes_)))
+        df[cat_col] = df[cat_col].map(le_dict)
+        np.save(path + '{}.npy'.format(cat_col), d)
+        return df
+    elif type == 'load':
+        print(cat_col)
         d = np.load(path + '{}.npy'.format(cat_col), allow_pickle=True).item()
         return d
 
