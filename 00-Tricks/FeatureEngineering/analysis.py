72e057af7ad0305ef356dda2fe536b8d08b97c40
michaelzhouy <15602409303@163.com>
2020-11-08 12:22:06 +0800

    Update analysis.py

--- 00-Tricks/FeatureEngineering/analysis.py
+++ 00-Tricks/FeatureEngineering/analysis.py
@@ -5,29 +5,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# 特征的缺失情况随着时间推移的变化
-p = 'f24'
-a = pd.DataFrame(data.groupby('ndays')[p].apply(lambda x: sum(pd.isnull(x))) / data.groupby('ndays')['ndays'].count()).reset_index()
-a.columns = ['ndays', p]
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-plt.plot(a['ndays'], a[p])
-plt.axvline(61, color='r')
-plt.axvline(122, color='r')
-plt.axvline(153, color='r')
-plt.xlabel('ndays')
-plt.ylabel('miss_rate_' + p)
-plt.title('miss_rate_' + p)
+def null_analysis(df, p='f24'):
+    """
+    特征的缺失情况随着时间推移的变化
+    @param df:
+    @param p:
+    @return:
+    """
+    a = pd.DataFrame(df.groupby('ndays')[p].apply(lambda x: sum(pd.isnull(x))) / df.groupby('ndays')['ndays'].count()).reset_index()
+    a.columns = ['ndays', p]
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.plot(a['ndays'], a[p])
+    plt.axvline(61, color='r')
+    plt.axvline(122, color='r')
+    plt.axvline(153, color='r')
+    plt.xlabel('ndays')
+    plt.ylabel('miss_rate_' + p)
+    plt.title('miss_rate_' + p)
 
 
-# 特征取值随着时间推移的变化
-a = pd.DataFrame(data.groupby('ndays')[p].mean()).reset_index()
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-plt.plot(a['ndays'], a[p])
-plt.axvline(61, color='r')
-plt.axvline(122, color='r')
-plt.axvline(153, color='r')
-plt.xlabel('ndays')
-plt.ylabel('mean_of_' + p)
-plt.title('distribution of ' + p)
+def value_analysis(df, p='f24'):
+    """
+    特征取值随着时间推移的变化
+    @param df:
+    @param p:
+    @return:
+    """
+    a = pd.DataFrame(df.groupby('ndays')[p].mean()).reset_index()
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    plt.plot(a['ndays'], a[p])
+    plt.axvline(61, color='r')
+    plt.axvline(122, color='r')
+    plt.axvline(153, color='r')
+    plt.xlabel('ndays')
+    plt.ylabel('mean_of_' + p)
+    plt.title('distribution of ' + p)
