9d6734bfcf8153dfd3e232433bae50ea3e157b35
Michaelzhouy <15602409303@163.com>
2021-06-06 23:28:52 +0800

    Update run.py

--- 12-DC/04-逾期风控预测/fk_model/model/run.py
+++ 12-DC/04-逾期风控预测/fk_model/model/run.py
@@ -33,146 +33,54 @@ def main(to_pred_dir, result_save_path):
     payment_b_path = os.path.join(to_pred_dir, "payment_b.csv")
     orders_b_path = os.path.join(to_pred_dir, "orders_b.csv")
     train_path = sys.path[0] + '/'
-    iot_a_path = train_path + "iot_a.csv"
-    payment_a_path = train_path + "payment_a.csv"
-    orders_a_path = train_path + "orders_a.csv"
 
-    payment_a = pd.read_csv(payment_a_path, index_col=None)
-    orders_a = pd.read_csv(orders_a_path, index_col=None)
-    iot_a = pd.read_csv(iot_a_path, index_col=None)
-    payment_b = pd.read_csv(payment_b_path, index_col=None)
-    orders_b = pd.read_csv(orders_b_path, index_col=None)
-    iot_b = pd.read_csv(iot_b_path, index_col=None)
+    payment_a = pd.read_csv(payment_b_path, index_col=None)
+    orders_a = pd.read_csv(orders_b_path, index_col=None)
+    iot_a = pd.read_csv(iot_b_path, index_col=None)
 
-    payment_a = payment_a.loc[payment_a['Y'].notnull(), :]
-    payment_a['Y'] = payment_a['Y'].map(int)
-    payment_a['is_test'] = 0
-    payment_b['is_test'] = 1
+    payment_a.sort_values(['customer_id', 'device_code', 'SSMONTH'], ascending=True, inplace=True)
 
-    payment = pd.concat([payment_a, payment_b], axis=0, copy=False)
-    orders = pd.concat([orders_a, orders_b], axis=0, copy=False)
-    iot = pd.concat([iot_a, iot_b], axis=0, copy=False)
-    iot.drop(['latitude', 'longitude'], axis=1, inplace=True)
-    iot['SSMONTH'] = iot['reporttime'].map(lambda x: int(x[:4] + x[5:7]))
-    iot['reporttime_min'] = iot.groupby(['device_code', 'SSMONTH'])['reporttime'].transform('min')
-    iot['reporttime_max'] = iot.groupby(['device_code', 'SSMONTH'])['reporttime'].transform('max')
-    iot_ym = iot.drop_duplicates(subset=["device_code", "SSMONTH", "reporttime_min", "reporttime_max"], keep="first")
-    iot_ym.drop('reporttime', axis=1, inplace=True)
-    tmp = iot.groupby(['device_code', 'SSMONTH'], as_index=False)['work_sum_time'].agg({
-        'work_sum_time_cnt': 'count',
-        'work_sum_time_mean': 'mean',
-        'work_sum_time_sum': 'sum',
-        'work_sum_time_min': 'min',
-        'work_sum_time_max': 'max',
-        'work_sum_time_std': 'std'
-    })
-    iot_ym = iot_ym.merge(tmp, on=['device_code', 'SSMONTH'], how='left')
+    # payment_a['year'] = payment_a['SSMONTH'].map(lambda x: int(str(x)[:4]))
+    payment_a['month'] = payment_a['SSMONTH'].map(lambda x: int(str(x)[4:6]))
+    payment_a['month_chunjie'] = payment_a['month'].map(lambda x: 1 if x in [1, 11, 12] else 0)
 
-    df1 = pd.merge(payment, orders, on=['device_code', 'customer_id'], how='left')
-    df = pd.merge(df1, iot_ym, on=['device_code', 'SSMONTH'], how='left')
-    print('数据合并完成!')
+    payment_a['DLSBH'] = payment_a['DLSBH'].map(lambda x: int(x[-2:]))
 
-    df['reporttime_min-posting_date'] = (
-            pd.to_datetime(df['reporttime_min']) - pd.to_datetime(df['posting_date'])).apply(lambda x: x.days)
-    df['reporttime_max-posting_date'] = (
-            pd.to_datetime(df['reporttime_max']) - pd.to_datetime(df['posting_date'])).apply(lambda x: x.days)
-    df['reporttime_max-reporttime_min'] = (
-            pd.to_datetime(df['reporttime_max']) - pd.to_datetime(df['reporttime_min'])).apply(lambda x: x.days)
-    df.drop(['posting_date', 'reporttime_min', 'reporttime_max'], axis=1, inplace=True)
-    df['DLSBH'] = df['DLSBH'].map(lambda x: int(x[-2:]))
-    df['QC/RZQS'] = df['QC'] / df['RZQS']
-    df['RZQS-QC'] = df['RZQS'] - df['QC']
-    # tmp = df.groupby(['customer_id', 'device_code'], as_index=False)['notified'].agg({
-    #     'notified_nunique': 'nunique',
-    #     'notified_cnt': 'count',
-    #     'notified_mean': 'mean',
-    #     'notified_sum': 'sum',
-    #     'notified_min': 'min',
-    #     'notified_max': 'max',
-    #     'notified_std': 'std'
-    # })
-    # df = df.merge(tmp, on=['customer_id', 'device_code'], how='left')
-    # # df[''] = df['notified_mean'] / df['work_sum_time_mean']
-    #
-    # tmp = df.groupby('device_code', as_index=False)['notified'].agg({
-    #     'device_code_notified_nunique': 'nunique',
-    #     'device_code_notified_cnt': 'count',
-    #     'device_code_notified_mean': 'mean',
-    #     'device_code_notified_sum': 'sum',
-    #     'device_code_notified_min': 'min',
-    #     'device_code_notified_max': 'max',
-    #     'device_code_notified_std': 'std'
-    # })
-    # df = df.merge(tmp, on='device_code', how='left')
-    #
-    # tmp = df.groupby('customer_id', as_index=False)['notified'].agg({
-    #     'customer_id_notified_nunique': 'nunique',
-    #     'customer_id_notified_cnt': 'count',
-    #     'customer_id_notified_mean': 'mean',
-    #     'customer_id_notified_sum': 'sum',
-    #     'customer_id_notified_min': 'min',
-    #     'customer_id_notified_max': 'max',
-    #     'customer_id_notified_std': 'std'
-    # })
-    # df = df.merge(tmp, on='customer_id', how='left')
+    payment_a['RZQS'] = payment_a['RZQS'].map(int)
+    payment_a['QC'] = payment_a['QC'].map(int)
 
-    train = df.loc[df['is_test'] == 0, :]
-    test = df.loc[df['is_test'] == 1, :]
-    cols = [col for col in train.columns if col not in ['device_code', 'customer_id', 'overdue', 'is_test']]
-    train_tr = train.loc[train['SSMONTH'] != 201904, :]
-    train_va = train.loc[train['SSMONTH'] == 201904, :]
-    X_train = train_tr[cols]
-    y_train = train_tr['Y']
-    X_valid = train_va[cols]
-    y_valid = train_va['Y']
-    X_test = test[cols]
+    payment_a['RZQS-QC'] = payment_a['RZQS'] - payment_a['QC']  # 剩余期次数
+    payment_a['QC/RZQS'] = payment_a['QC'] / payment_a['RZQS']  # 当前期次 / 融资期数
 
-    cat_cols = ['DLSBH', 'SSMONTH', 'RZQS']
+    # payment_a['notified_shift_1'] = payment_a.groupby(['customer_id', 'device_code'])['notified'].shift(1)
+    # payment_a['notified_shift_2'] = payment_a.groupby(['customer_id', 'device_code'])['notified'].shift(2)
+    # payment_a['notified_2'] = payment_a['notified'] + payment_a['notified_shift_1']
+    payment_a['notified_shift_-1'] = payment_a.groupby(['customer_id', 'device_code'])['notified'].shift(-1)
+    payment_a['notified_shift_-2'] = payment_a.groupby(['customer_id', 'device_code'])['notified'].shift(-2)
+    # fill = {'notified_shift_1': 1, 'notified_shift_2': 1}
+    # payment_a.fillna(fill, inplace=True)
 
-    dtrain = lgb.Dataset(X_train, y_train)
-    dvalid = lgb.Dataset(X_valid, y_valid, reference=dtrain)
+    payment_a['customer_id_notified_times'] = payment_a.groupby('customer_id')['notified'].transform('mean')
+    payment_a['device_code_notified_times'] = payment_a.groupby('device_code')['notified'].transform('mean')
+    payment_a['device_code_customer_id_notified_times'] = payment_a.groupby(['device_code', 'customer_id'])[
+        'notified'].transform('mean')
+    payment_a['DLSBH_customer_id_notified_times'] = payment_a.groupby(['DLSBH', 'customer_id'])['notified'].transform(
+        'mean')
+    payment_a['DLSBH_device_code_notified_times'] = payment_a.groupby(['DLSBH', 'device_code'])['notified'].transform(
+        'mean')
 
-    params = {
-        'objective': 'binary',
-        'boosting': 'gbdt',
-        'metric': 'auc',
-        # 'metric': 'None',  # 用自定义评估函数是将metric设置为'None'
-        'learning_rate': 0.1,
-        'num_leaves': 31,
-        'lambda_l1': 0,
-        'lambda_l2': 1,
-        'num_threads': 23,
-        'min_data_in_leaf': 20,
-        'first_metric_only': True,
-        'is_unbalance': True,
-        'max_depth': -1,
-        'seed': 2021
-    }
-
-    # def lgb_f1_score(y_hat, data):
-    #     y_true = data.get_label()
-    #     y_hat = np.where(y_hat >= 0.5, 1, 0)
-    #     return 'f1', f1_score(y_true, y_hat), True
-
-    model = lgb.train(
-        params,
-        dtrain,
-        valid_sets=[dvalid, dtrain],
-        num_boost_round=1000000,
-        early_stopping_rounds=200,
-        verbose_eval=300,
-        # feval=lgb_f1_score,
-        categorical_feature=cat_cols
-    )
+    X_test = payment_a.drop(['device_code', 'customer_id', 'overdue', 'Y'], axis=1)
 
     # cwd = sys.argv[0]
     # model = lgb.Booster(model_file=os.path.join(cwd[:-6], f'lgb.txt'))
+    train_path = sys.path[0] + '/'
+    model = lgb.Booster(model_file=train_path + 'lgb.txt')
     y_pred = model.predict(X_test, num_iteration=model.best_iteration)
     print('predict Done!')
 
     y_pred = np.where(y_pred >= 0.5, 1, 0)
 
-    __result = test.loc[:, ["SSMONTH", "device_code", "customer_id"]]
+    __result = payment_a.loc[:, ["SSMONTH", "device_code", "customer_id"]]
     __result["Y"] = y_pred
     # __result.sort_values(by="SSMONTH",ascending=False,inplace=True)
     result = __result[__result["SSMONTH"] == 201904]
@@ -188,8 +96,6 @@ def main(to_pred_dir, result_save_path):
 # 以下代码不得修改, 若修改以下代码将会导致无法计算得分。
 ===========================================================
 """
-
-# os.path.join(sys.argv[0][:-6], f'model_{i}.txt')
 
 
 if __name__ == "__main__":
